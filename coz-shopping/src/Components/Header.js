fb9a032a263443191f9c6dab20d3635356beea80
sintobury <herkongho@gmail.com>
2023-07-11 16:38:00 +0900

    헤더 css

--- coz-shopping/src/Components/Header.js
+++ coz-shopping/src/Components/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Header.css';
 
 const Header = ( ) => {
 
     return (
         <header>
             <section className="left_section">
-                <img src= "img/로고.png"></img>
+                <img src= "img/로고.png" className="logo"></img>
                 <h1>COZ Shopping</h1>
             </section>
             <section className="right_section">
-                <img src="img/아이콘.png"></img>
+                <img src="img/아이콘.png" className="hamburger"></img>
             </section>
         </header>
     )
