2b54c189b1bafd7c76c9d0dd0d417f0b970da86e
sintobury <herkongho@gmail.com>
2023-07-12 10:52:21 +0900

    footer완성

---
+++ coz-shopping/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">개인정보 처리방침 | 이용 약관</div>
+            <div className="footer">All rights reserved @ Codestates</div>
+        </footer>
+    )
+}
+
+export default Footer
