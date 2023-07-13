9808360658d53f50999ad6241e8a265cb9b00087
sintobury <herkongho@gmail.com>
2023-07-13 11:28:07 +0900

    북마크리스트 추가기능구현

--- coz-shopping/src/Components/Productitem.js
+++ coz-shopping/src/Components/Productitem.js
@@ -1,17 +1,27 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import './item.css'
 
 const ProductItem = ({data, idx, addBookmark ,list}) => {
     //addBookmark= setBookmarkList
+    const [clicked, setClicked] = useState(false);
+
     const handleBookmark = () => {
         addBookmark([...list, data[idx]]);
+        setClicked(!clicked);
     }
+    const deleteBookmark = () => {
+        addBookmark([...list.filter((el)=> el!==data[idx])]);
+        setClicked(!clicked);
+    }
+
     const setBookmarkList = () => {
-        window.localStorage.setItem('bookmarklist', list);
+        window.localStorage.setItem('bookmarklist', JSON.stringify(list));
+        console.log(JSON.parse(window.localStorage.getItem('bookmarklist')))
     }
     useEffect(() => {
         setBookmarkList();
     }, [list]);
+    
 
     if(data.length === 0){
         return 
@@ -21,7 +31,8 @@ const ProductItem = ({data, idx, addBookmark ,list}) => {
             <div>
                 <div className="img_container">
                     <img src={data[idx].image_url} alt='' className="background"></img>
-                    <img src="img/북마크 아이콘.png" className="bookmark" onClick={handleBookmark}></img>
+                    <img src="img/북마크 아이콘.png" className={!clicked ? "bookmark" : 'hide'} onClick={handleBookmark}></img>
+                    <img src="img/북마크됨 아이콘.png" className={clicked ? "bookmark": 'hide'} onClick={deleteBookmark}></img>
                 </div>
             </div>
             <div className="item_content">
