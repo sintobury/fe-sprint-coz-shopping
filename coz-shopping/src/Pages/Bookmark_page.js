4e73d8d0826e0a507b608257699f57cbacc30051
sintobury <herkongho@gmail.com>
2023-07-14 16:40:17 +0900

    북마크 페이지 시작

---
+++ coz-shopping/src/Pages/Bookmark_page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import ProductItem from "../Components/Productitem";
+import CategoryItem from "../Components/CategoryItem";
+import ExhibitionItem from "../Components/ExhibitionItem";
+import BrandItem from "../Components/BrandItem";
+import Tab from "../Components/Tab";
+import "./Bookmark_page.css"
+
+const BookmarkList_page = () => {
+    return (
+        <div className="BookmarkList_page">
+            <Header/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default BookmarkList_page
